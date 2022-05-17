b8ad3937302f134934d0b9cb495ebde7bbd895b2
agustinDelpane <90353373+agustinDelpane@users.noreply.github.com>
2022-05-17 17:12:03 -0300

    Add files via upload

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Badge}  from 'react-bootstrap'
+import './CartWidget.css'
+
+function CartWidget() {
+  return (
+    <>
+    <img src={require('../../assets/img/cart.png')} className='cartPng' alt='Cart' /> <Badge pill bg="success" className='badge4'>
+    4
+  </Badge>{' '}
+    </>
+    )
+}
+
+export default CartWidget
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import {Navbar, Container, Nav, NavDropdown} from 'react-bootstrap'
+import CartWidget from '../CartWidget/CartWidget'
+import './NavBar.css'
+
+const NavBar = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Container>
+    <Navbar.Brand href="#home">Kamino Games</Navbar.Brand>
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+        <Nav.Link href="#novedades">Novedades</Nav.Link>
+        <Nav.Link href="#ofertas">Ofertas</Nav.Link>
+        <Nav.Link href="#mas-vendidos">Más vendidos</Nav.Link>
+        <NavDropdown title="Consolas" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="#consolas/pc">PC</NavDropdown.Item>
+            <NavDropdown.Item href="#consolas/ps4">PS4</NavDropdown.Item>
+            <NavDropdown.Item href="#consolas/xbone">Xbox One</NavDropdown.Item>
+            <NavDropdown.Item href="#consolas/nswitch">Nintendo Switch</NavDropdown.Item>
+        </NavDropdown>
+        </Nav>
+        <Nav>
+        <Nav.Link href="#sobre-nosotros">Sobre nosotros</Nav.Link>
+        <Nav.Link href="#contacto">Contacto</Nav.Link>
+        </Nav>
+        <CartWidget />
+    </Navbar.Collapse>
+    </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar
