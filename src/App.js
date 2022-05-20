8adc6d264d615e26ed386cef02d1193481a0b3d0
agustinDelpane <90353373+agustinDelpane@users.noreply.github.com>
2022-05-20 17:39:52 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import './App.css';
 import NavBar from './components/NavBar/NavBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ItemListContainer from './components/ItemListContainer';
-import ItemCount from './components/ItemCount/ItemCount';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <ItemListContainer greet='Hola, bienvenido!' />
-      <ItemCount initial={1} stock={5} />
     </div>
   );
 }
--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ItemCount from './ItemCount/ItemCount'
 
 const ItemListContainer = ({ greet })  => {
   return (
     <div>
         <h1>{ greet } </h1>
+        <ItemCount initial={1} stock={5} />
     </div>
   )
 }
